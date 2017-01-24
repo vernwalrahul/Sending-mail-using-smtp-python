3504d948c5719ceccf3eeba44f17164f50b4895d
vernwalrahul <vernwalrahul@gmail.com>
2017-01-24 09:47:44 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,5 +1,6 @@
 import smtplib
 import socks
+from smtplib import SMTPAuthenticationError
 
 fromaddr = input('sender email: ')
 toaddrs  = input('receiver email: ')
@@ -7,10 +8,15 @@ msg = input('message')
 username = input('your email: ')
 password = input('your password: ')
 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-print('email sent')
+try:
+ server = smtplib.SMTP('smtp.gmail.com:587')
+ server.ehlo()
+ server.starttls()
+ server.login(username,password)
+ server.sendmail(fromaddr, toaddrs, msg)
+ print('email sent')
+except SMTPAuthenticationError:
+    print('Error: \n Possible Reasons: \n->invalid username/pswd \n->Login using less secured app has been turned off.')
+    
+
 server.quit()
